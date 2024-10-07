248ca2b3b1a626c3ab097cde0327f995b609cc39
juh22jjuh <anajuliamartins468@gmail.com>
2024-10-07 08:42:11 -0300

    home

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "../../../shared/ui/carousel/carousel.component";
 import { NavBarComponent } from "../../../shared/ui/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../../shared/ui/footer/footer.component";
+import { CardComponent } from "../../../shared/ui/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, NavBarComponent],
+  imports: [CarouselComponent, NavBarComponent, FooterComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
